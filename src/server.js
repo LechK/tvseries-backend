fe62f54ac4048c79127c0c3aae8e3ce19bd62951
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-08 20:19:02 +0200

    minor router fix (forgot import)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const router = require("./routes");
 require("dotenv").config();
 
 const port = process.env.SERVER_PORT || 3000;
