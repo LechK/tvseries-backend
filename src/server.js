f3ebf458e42464201064d891048409994edf9986
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-08 20:17:27 +0200

    router added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mysql = require("mysql");
 require("dotenv").config();
 
 const port = process.env.SERVER_PORT || 3000;
@@ -10,9 +9,6 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(router);
 
-app.get("/", (req, res) => {
-  res.send("This boilerplate is working!");
-});
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is rolling on port ${port}`));
