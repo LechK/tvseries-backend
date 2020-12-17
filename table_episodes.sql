8da3fe78ca6d09261ff18e52a258f8c1771ce483
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-17 18:28:58 +0200

    table episodes added episode_title

--- table_episodes.sql
+++ table_episodes.sql
@@ -1,1 +1,1 @@
-CREATE TABLE episodes (id INT PRIMARY KEY AUTO_INCREMENT, season_id INT, order_num INT)
+CREATE TABLE episodes (id INT PRIMARY KEY AUTO_INCREMENT, season_id INT, order_num INT, episode_title TEXT)
