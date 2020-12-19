58a7ab6686be0069605c9d1fb8a10fca22262d6f
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-19 22:02:28 +0200

    added column to episodes table

--- table_episodes.sql
+++ table_episodes.sql
@@ -1,1 +1,1 @@
-CREATE TABLE episodes (id INT PRIMARY KEY AUTO_INCREMENT, season_id INT, order_num INT, episode_title TEXT)
+CREATE TABLE episodes (id INT PRIMARY KEY AUTO_INCREMENT, season_id INT, order_num INT, episode_title TEXT, tv_series_id INT)
